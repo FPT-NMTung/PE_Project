9ec4d9c43ad76028a57e254d3bf95a9540df0f35
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2021-04-26 08:26:40 +0700

    update config database

--- model/index.js
+++ model/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connect() {
+    // connetion string : mongodb+srv://user_website:NMTung88644264@cluster0.nyv2d.mongodb.net/test?authSource=admin&replicaSet=atlas-k3ridv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
     try {
-        await mongoose.connect('mongodb+srv://user_website:NMTung88644264@cluster0.nyv2d.mongodb.net/dev-pe-project?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb://localhost:27017/pe_project_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
